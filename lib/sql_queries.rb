2be2437cff2450d6b667e4264512560ed46ceb1d
Kevin Lopez Cifuentes <us.kevin@icloud.com>
2019-03-22 08:45:41 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -1,5 +1,5 @@
 def selects_all_female_bears_return_name_and_age
-  "SELECT name, age FROM bears WHERE gender == 'F';"
+  "SELECT name, age FROM bears WHERE gender = 'F';"
 end
 
 def selects_all_bears_names_and_orders_in_alphabetical_order
@@ -19,12 +19,11 @@ def select_youngest_bear_and_returns_name_and_age
 end
 
 def selects_most_prominent_color_and_returns_with_count
-  # SELECT name,COUNT(*) as count FROM tablename GROUP BY name ORDER BY count DESC;
   "SELECT color,COUNT(*) as COUNT FROM bears GROUP BY color ORDER BY count DESC LIMIT 1;"
 end
 
 def counts_number_of_bears_with_goofy_temperaments
- "SELECT COUNT(*) FROM bears WHERE temperament == 'goofy'"
+ "SELECT COUNT(*) FROM bears WHERE temperament = 'goofy'"
 end
 
 def selects_bear_that_killed_Tim
